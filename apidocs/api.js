191b1cf9d37fe421985be50b02af68c52d685775
Chad Auld <chadauld@gmail.com>
2016-08-15 12:53:43 -0600

    Updating grunt setup to handle linting and doc gen (githook now calls grunt)

---
+++ apidocs/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [],
+    "modules": [
+        "nextract"
+    ],
+    "allModules": [
+        {
+            "displayName": "nextract",
+            "name": "nextract",
+            "description": "Nextract module...."
+        }
+    ],
+    "elements": []
+} };
+});
