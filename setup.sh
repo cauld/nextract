cf32eca585f33e51508e0bdba574607a637ab5f7
Chad Auld <chadauld@gmail.com>
2016-08-17 11:51:42 -0600

    Updating setup

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ echo "Installing required npm packages..."
 npm install
 
 echo "Excluding config file changes from git index to prevent committing sensitive data..."
-git rm --cached "/src/plugins/config/default.json" > /dev/null 2>&1
-git rm --cached "/build/plugins/config/default.json" > /dev/null 2>&1
+git rm --cached "src/plugins/config/default.json" > /dev/null 2>&1
+git rm --cached "build/plugins/config/default.json" > /dev/null 2>&1
