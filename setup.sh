308bdc511af5f916dccff49465e4dd85c830961e
Chad Auld <chadauld@gmail.com>
2016-08-17 12:05:44 -0600

    Updating gitignore

--- setup.sh
+++ setup.sh
@@ -3,6 +3,5 @@
 echo "Installing required npm packages..."
 npm install
 
-echo "Excluding config file changes from git index to prevent committing sensitive data..."
-git rm --cached "src/plugins/config/default.json" > /dev/null 2>&1
-git rm --cached "build/plugins/config/default.json" > /dev/null 2>&1
+echo "Add default config files..."
+node utils/add_default_config.js
---
+++ utils/add_default_config.js
@@ -0,0 +1,34 @@
+var fs = require('fs'),
+    jsonfile = require('jsonfile');
+
+var defaultConfig,
+    baseConfigFileLocation
+
+baseConfigFileLocation = '/plugins/config/default.json';
+
+defaultConfig = {
+  "logging": {
+    "logFilePath": "/var/log/nextract.log"
+  },
+
+  "databases": {
+    "nextract_sample": {
+      "dialect": "mysql",
+      "host": "127.0.0.1",
+      "port": 3306,
+      "name": "nextract",
+      "user": "root",
+      "password": ""
+    }
+  }
+};
+
+jsonfile.writeFile('src/' + baseConfigFileLocation, defaultConfig, {spaces:2}, function (err) {
+  if (err) {
+    console.error("Unable to write default src config file!");
+  } else {
+    //Copy over to the build area as well since non-devs won't run "grunt dev"
+    fs.createReadStream('src/' + baseConfigFileLocation)
+      .pipe(fs.createWriteStream('build/' + baseConfigFileLocation));
+  }
+});
